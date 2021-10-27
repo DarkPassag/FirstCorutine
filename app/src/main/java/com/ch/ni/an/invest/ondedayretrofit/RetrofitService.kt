02694cb75856c722923b19e3d0226ee67992539b
Nikita <cherskih2@gmail.com>
2021-10-27 21:30:18 +1000

    simple bad codding and add clickListener

--- app/src/main/java/com/ch/ni/an/invest/ondedayretrofit/RetrofitService.kt
+++ app/src/main/java/com/ch/ni/an/invest/ondedayretrofit/RetrofitService.kt
@@ -1,32 +1,21 @@
 package com.ch.ni.an.invest.ondedayretrofit
 
 import com.ch.ni.an.invest.model.AnimeChan
-import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
-
-const val BASE_URL = "https://animechan.vercel.app/api/"
+import retrofit2.http.Path
 
 interface RetrofitService {
 
     @GET("quotes")
-    suspend fun getRandomQuotes(): Response<List<AnimeChan>>
+    suspend fun getBasicQuotes(): List<AnimeChan>
 
-    @GET("random")
-    suspend fun getRandomQuote(): AnimeChan
+    @GET("quotes/anime?title=naruto")
+    suspend fun getQuotesByAnimeName(): List<AnimeChan>
 
-    @GET("available/anime")
-    suspend fun getAvailableAnime(): Response<List<String>>
-
-    @GET("quotes/anime?")
-    suspend fun getQuotesByAnime(@Query("title" )animeName: String): List<AnimeChan>
-
-    @GET("quotes/character?")
-    suspend fun getQuotesByCharacter(@Query("name") characterName: String): Response<List<AnimeChan>>
 }
-
 object Common{
+    val retrofit: RetrofitService
+        get() =  RetrofitClient.getClient().create(RetrofitService::class.java)
 
-    val retrofit = RetrofitClient.getClient(BASE_URL).create(RetrofitService::class.java)
 
 }
--- app/src/main/java/com/ch/ni/an/invest/screens/FragmentAnimeNameQuotes.kt
+++ app/src/main/java/com/ch/ni/an/invest/screens/FragmentAnimeNameQuotes.kt
@@ -1,6 +1,5 @@
 package com.ch.ni.an.invest.screens
 
-import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,13 +12,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ch.ni.an.invest.AnimeAdapter
 import com.ch.ni.an.invest.databinding.FragmentAnimenameQuotesBinding
 import com.ch.ni.an.invest.ondedayretrofit.AnimeViewModel
-import com.ch.ni.an.invest.ondedayretrofit.STATE
 
 class FragmentAnimeNameQuotes:Fragment() {
 
     private var _bind: FragmentAnimenameQuotesBinding? = null
-    private val bind: FragmentAnimenameQuotesBinding
-        get() = _bind!!
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: AnimeAdapter
     private val myModel: AnimeViewModel by activityViewModels()
@@ -30,7 +26,7 @@ class FragmentAnimeNameQuotes:Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _bind = FragmentAnimenameQuotesBinding.inflate(inflater, container, false)
-
+        val bind = _bind!!
 
         recyclerView = bind.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -40,42 +36,15 @@ class FragmentAnimeNameQuotes:Fragment() {
             }
         })
         recyclerView.adapter = adapter
-        backgroundAnimation()
         return bind.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        myModel.state.observe(viewLifecycleOwner, {
-            when(it){
-                STATE.PENDING -> pending()
-                STATE.SUCCESS -> updateUI()
-                STATE.FAIL -> updateUI()
-            }
-        })
-
+        myModel.getQuotesByAnime1()
         myModel.animeQuotes.observe(viewLifecycleOwner, {
             adapter.animeList = it
             recyclerView.adapter = adapter
         })
-    }
-
-    private fun backgroundAnimation(){
-        val animationDrawable = bind.recyclerView.background as AnimationDrawable
-        animationDrawable.apply {
-            setEnterFadeDuration(1000)
-            setExitFadeDuration(3000)
-            start()
-        }
-    }
-
-    private fun updateUI(){
-        bind.dotsLoaderProgressbar.visibility = View.GONE
-        bind.recyclerView.visibility = View.VISIBLE
-    }
-    private fun pending(){
-       bind.dotsLoaderProgressbar.visibility = View.VISIBLE
-       bind.recyclerView.visibility = View.GONE
     }
 }
