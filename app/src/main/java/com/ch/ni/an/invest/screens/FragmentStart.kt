b09b561cef32aada2b6e0ee7e2fe727a56694f10
Nikita <cherskih2@gmail.com>
2021-11-01 09:49:54 +1000

    Add dot

--- app/src/main/java/com/ch/ni/an/invest/screens/FragmentStart.kt
+++ app/src/main/java/com/ch/ni/an/invest/screens/FragmentStart.kt
@@ -1,6 +1,5 @@
 package com.ch.ni.an.invest.screens
 
-import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,18 +9,17 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.ch.ni.an.invest.ListAnimeAdapter
+import com.ch.ni.an.invest.AnimeAdapter
 import com.ch.ni.an.invest.R
 import com.ch.ni.an.invest.databinding.FragmentStartBinding
 import com.ch.ni.an.invest.ondedayretrofit.AnimeViewModel
-import com.ch.ni.an.invest.ondedayretrofit.STATE
 
 
 class FragmentStart: Fragment() {
     private lateinit var bind: FragmentStartBinding
     private val myModel: AnimeViewModel by activityViewModels()
     private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: ListAnimeAdapter
+    private lateinit var adapter: AnimeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,56 +29,26 @@ class FragmentStart: Fragment() {
         bind = FragmentStartBinding.inflate(inflater,container,false)
         recyclerView = bind.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        adapter = ListAnimeAdapter(object : RecyclerViewClickListener{
+        adapter = AnimeAdapter(object : RecyclerViewClickListener{
             override fun clickListener(animeName: String) {
-                myModel.getQuotesByAnime(animeName)
                 findNavController().navigate(R.id.action_fragmentStart_to_fragmentAnimeNameQuotes)
+                myModel.setAnimeName(animeName)
             }
         })
         recyclerView.adapter = adapter
-        backgroundAnimation()
+
         return bind.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        myModel.state.observe(viewLifecycleOwner, {
-            when(it){
-                STATE.PENDING -> pending()
-                STATE.SUCCESS -> updateUI()
-                STATE.FAIL -> updateUI()
-            }
-        })
-
-        myModel.listAvailableAnime.observe(viewLifecycleOwner, {
-            it?.let {
-                adapter.listAnime = it
-                recyclerView.adapter = adapter
-            }
-
+        myModel.basicQuotes.observe(viewLifecycleOwner, {
+            adapter.animeList = it
+            recyclerView.adapter = adapter
         })
 
 
 
-    }
-
-
-    private fun backgroundAnimation(){
-        val animatorDrawable = bind.recyclerView.background as AnimationDrawable
-        animatorDrawable.apply {
-            setEnterFadeDuration(1000)
-            setExitFadeDuration(3000)
-            start()
-        }
-    }
-
-    private fun updateUI(){
-        bind.dotsLoaderProgressbar.visibility = View.GONE
-        bind.recyclerView.visibility = View.VISIBLE
-    }
-    private fun pending(){
-        bind.dotsLoaderProgressbar.visibility = View.VISIBLE
-        bind.recyclerView.visibility = View.GONE
     }
 }
