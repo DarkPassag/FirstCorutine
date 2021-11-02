ea243bba0baf5e903acfc14ba03a1ee21514c24d
Nikita <cherskih2@gmail.com>
2021-11-02 22:03:32 +1000

    Add fragment with my quotes

--- app/src/main/java/com/ch/ni/an/invest/MainActivity.kt
+++ app/src/main/java/com/ch/ni/an/invest/MainActivity.kt
@@ -4,11 +4,14 @@ package com.ch.ni.an.invest
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.ch.ni.an.invest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,8 +23,21 @@ class MainActivity : AppCompatActivity() {
         bind = ActivityMainBinding.inflate(layoutInflater)
         backGroundColor()
         setContentView(bind.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 
+    override fun onCreateOptionsMenu(menu :Menu?) :Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item :MenuItem) :Boolean {
+//        when(item.itemId){
+//
+//        }
+        return true
+    }
     private fun backGroundColor() {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = ContextCompat.getColor(this, android.R.color.transparent)
