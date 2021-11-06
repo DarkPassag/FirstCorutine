19430bb3772f474d74f6be2e77dae13dc3bcd3f7
Nikita <cherskih2@gmail.com>
2021-11-06 22:40:55 +1000

    add image

---
+++ app/src/main/java/com/ch/ni/an/invest/model/retrofit/RetrofitClientImage.kt
@@ -0,0 +1,18 @@
+package com.ch.ni.an.invest.model.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+object RetrofitClientImage {
+    private var retrofit: Retrofit? = null
+
+    fun getClient(url: String): Retrofit{
+        if(retrofit == null){
+            retrofit = Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build()
+        }
+        return retrofit!!
+    }
+}
